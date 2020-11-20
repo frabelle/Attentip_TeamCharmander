c36d9c7e040b83baf041174e35894ed377566e2b
frabelle <renchifrabelle@gmail.com>
2020-11-20 13:52:08 -0600

    Pequeños ajustes Parte 1

--- app/src/main/java/com/example/attentipteamcharmander/MainActivity.java
+++ app/src/main/java/com/example/attentipteamcharmander/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, HistorialEstadoA.class);
+        Intent intent = new Intent(this, EstadoAnimo.class);
         startActivity(intent);
     }
 }

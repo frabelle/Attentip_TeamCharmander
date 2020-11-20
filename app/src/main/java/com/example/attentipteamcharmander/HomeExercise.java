8f70a0707fa91777a61827ad72959f68b845d64b
frabelle <66578327+frabelle@users.noreply.github.com>
2020-11-20 00:54:08 -0600

    Segunda parte del diseño de interfaces (#2)

--- app/src/main/java/com/example/attentipteamcharmander/HomeExercise.java
+++ app/src/main/java/com/example/attentipteamcharmander/HomeExercise.java
@@ -2,17 +2,13 @@ package com.example.attentipteamcharmander;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeExercise extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Intent intent = new Intent(this, HistorialEstadoA.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_home_exercise);
     }
 }

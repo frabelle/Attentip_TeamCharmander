fc34aa7dd0def9277ea7d5a7c73dd9374d1b115d
Nicold Carcache <nicoldcarcache@gmail.com>
2020-12-04 05:56:46 -0600

    Btn de pausa activas

--- app/src/main/java/com/example/attentipteamcharmander/fragments/EjerciciosFisicosFragment.java
+++ app/src/main/java/com/example/attentipteamcharmander/fragments/EjerciciosFisicosFragment.java
@@ -49,54 +49,5 @@ public class EjerciciosFisicosFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_ejercicios_fisicos, container, false);
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        setup(view);
-    }
-
-    private void setup(@NonNull View view) {
-        setupinit(view);
-    }
-
-    private void setupinit(View view) {
-        MaterialButton btn1, btn2, btn3;
-
-        btn1 = view.findViewById(R.id.btnFd);
-
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openFragment(EjercicioInferiorFragment.newInstance());
-            }
-        });
-
-        btn2 = view.findViewById(R.id.btnAc);
-
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openFragment(EjercicioAbsTroncoFragment.newInstance());
-            }
-        });
-
-        btn3 = view.findViewById(R.id.btnMa);
-
-        btn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openFragment(EjercicioCuelloFragment.newInstance());
-            }
-        });
-    }
-
-    public void openFragment(Fragment fragment) {
-        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.fragment, fragment);
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();
-    }
-
 
 }
--- app/src/main/java/com/example/attentipteamcharmander/fragments/PausaFisicaFragment.java
+++ app/src/main/java/com/example/attentipteamcharmander/fragments/PausaFisicaFragment.java
@@ -2,13 +2,21 @@ package com.example.attentipteamcharmander.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.attentipteamcharmander.R;
+import com.example.attentipteamcharmander.fragments.fragmentsFisica.EjercicioAbsTroncoFragment;
+import com.example.attentipteamcharmander.fragments.fragmentsFisica.EjercicioCuelloFragment;
+import com.example.attentipteamcharmander.fragments.fragmentsFisica.EjercicioInferiorFragment;
+import com.google.android.material.button.MaterialButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -38,5 +46,55 @@ public class PausaFisicaFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pausa_fisica, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setup(view);
+    }
+
+    private void setup(@NonNull View view) {
+        setupinit(view);
+    }
+
+    private void setupinit(View view) {
+        MaterialButton btn1, btn2, btn3;
+
+        btn1 = view.findViewById(R.id.btnFd);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFragment(EjercicioInferiorFragment.newInstance());
+            }
+        });
+
+        btn2 = view.findViewById(R.id.btnAc);
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFragment(EjercicioAbsTroncoFragment.newInstance());
+            }
+        });
+
+        btn3 = view.findViewById(R.id.btnMa);
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFragment(EjercicioCuelloFragment.newInstance());
+            }
+        });
+    }
+
+    public void openFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 }
